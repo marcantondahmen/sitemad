d519d73c9b63aa1c7c3c277a750192353eabb0b5
Marc Anton Dahmen <mail@marcdahmen.de>
2021-11-22 22:05:56 +0100

    style: apply formatting

---
+++ .php-cs-fixer.php
@@ -0,0 +1,73 @@
+<?php
+
+return (new PhpCsFixer\Config())
+	->setRules(array(
+		'@PSR2' => true,
+		'align_multiline_comment' => array('comment_type' => 'all_multiline'),
+		'array_indentation' => true,
+		'array_syntax' => array('syntax' => 'long'),
+		'blank_line_after_opening_tag' => false,
+		'blank_line_before_statement' => true,
+		'braces' => array(
+			'allow_single_line_closure' => true,
+			'position_after_functions_and_oop_constructs' => 'same'
+		),
+		'concat_space' => array(
+			'spacing' => 'one'
+		),
+		'ordered_class_elements' => array(
+			'sort_algorithm' => 'alpha'
+		),
+		'ordered_imports' => true,
+		'function_typehint_space' => true,
+		'method_argument_space' => array(
+			'on_multiline' => 'ensure_fully_multiline',
+			'after_heredoc' => true
+		),
+		'multiline_comment_opening_closing' => true,
+		'no_blank_lines_after_class_opening' => false,
+		'no_blank_lines_after_phpdoc' => true,
+		'no_closing_tag' => true,
+		'no_extra_blank_lines' => array(
+			'tokens' => array(
+				'curly_brace_block',
+				'extra',
+				'parenthesis_brace_block',
+				'square_brace_block',
+				'throw',
+				'use',
+			)
+		),
+		'no_spaces_after_function_name' => true,
+		'no_trailing_comma_in_list_call' => true,
+		'no_trailing_comma_in_singleline_array' => true,
+		'no_trailing_whitespace' => true,
+		'no_whitespace_before_comma_in_array' => true,
+		'no_whitespace_in_blank_line' => true,
+		'nullable_type_declaration_for_default_null_value' => array(
+			'use_nullable_type_declaration' => true
+		),
+		'phpdoc_add_missing_param_annotation' => array(
+			'only_untyped' => false
+		),
+		'phpdoc_align' => array(
+			'align' => 'left'
+		),
+		'phpdoc_indent' => true,
+		'phpdoc_order' => true,
+		'phpdoc_scalar' => true,
+		'phpdoc_trim' => true,
+		'phpdoc_trim_consecutive_blank_line_separation' => true,
+		'return_type_declaration' => true,
+		'phpdoc_types' => true,
+		'phpdoc_types_order' => array(
+			'null_adjustment' => 'always_last'
+		),
+		'single_blank_line_before_namespace' => true,
+		'single_blank_line_at_eof' => true,
+		'single_line_after_imports' => true,
+		'single_quote' => true
+	))
+	->setIndent("\t")
+	->setLineEnding("\n")
+;
--- src/sitemad.php
+++ src/sitemad.php
@@ -1,32 +1,27 @@
 <?php
 /**
  *	Sitemad
- *	
+ *
  *	Copyright 2020 by Marc Anton Dahmen
  *	https://marcdahmen.de
  *	MIT License
  */
-
 class Sitemad {
-	
-
-	private $config;
-
-	private $serverName;
-
-	private $serverType;
-	
-	private $phpVersion;
-	
 	private $baseDir;
 
 	private $baseUrl;
 
+	private $config;
+
+	private $phpVersion;
+
+	private $serverName;
+
+	private $serverType;
+
 	private $sitemadUrl;
 
-
 	public function __construct() {
-
 		$this->serverName = getenv('SERVER_NAME');
 		$this->serverType = preg_replace('/\/.*/', '', getenv('SERVER_SOFTWARE'));
 		$this->phpVersion = phpversion();
@@ -36,32 +31,19 @@ class Sitemad {
 		$this->config = $this->getConfig();
 
 		echo $this->generateOutput();
-
 	}
 
+	private function generateOutput() {
+		$rows = $this->generateRows();
 
-	private function getConfig() {
-
-		$defaults = json_decode(file_get_contents(dirname(__DIR__) . '/defaults.json'), true);
-		$config = array();
-		$configFile = dirname(__DIR__) . '/config.json';
-
-		if (is_readable($configFile)) {
-			$config = json_decode(file_get_contents($configFile), true);
-		}
-
-		return (object) array_merge($defaults, $config);
-
+		return require __DIR__ . '/templates/page.php';
 	}
-
 
 	private function generateRows() {
-
 		$sites = $this->scan($this->baseDir);
 		$html = '';
 
 		foreach ($sites as $path) {
-
 			$url = $this->urlFromPath($path);
 			$separator = '<svg class="text-muted" xmlns="http://www.w3.org/2000/svg" width="0.7em" height="0.7em" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/></svg>';
 			$text = str_replace('/', " $separator ", trim($url, '/'));
@@ -81,25 +63,24 @@ class Sitemad {
 			}
 
 			$html .= require __DIR__ . '/templates/row.php';
-
 		}
 
 		return $html;
-
 	}
 
+	private function getConfig() {
+		$defaults = json_decode(file_get_contents(dirname(__DIR__) . '/defaults.json'), true);
+		$config = array();
+		$configFile = dirname(__DIR__) . '/config.json';
 
-	private function generateOutput() {
+		if (is_readable($configFile)) {
+			$config = json_decode(file_get_contents($configFile), true);
+		}
 
-		$rows = $this->generateRows();
-
-		return require __DIR__ . '/templates/page.php';
-
+		return (object) array_merge($defaults, $config);
 	}
-
 
 	private function scan($dir, $depth = 0) {
-		
 		$sites = array();
 
 		if ($depth > $this->config->maxdepth) {
@@ -113,31 +94,23 @@ class Sitemad {
 				}
 			}
 		}
-		
+
 		$items = glob("$dir/*", GLOB_ONLYDIR);
 
 		if (is_array($items)) {
-
-			$items = array_filter($items, function($item) {
+			$items = array_filter($items, function ($item) {
 				return !preg_match('/(' . $this->config->ignore . ')/', $item);
 			});
 
 			foreach ($items as $item) {
 				$sites = array_merge($sites, $this->scan($item, $depth + 1));
 			}
-
 		}
-		
+
 		return $sites;
-
 	}
-
 
 	private function urlFromPath($path) {
-
 		return preg_replace('/^' . preg_quote($this->baseDir, '/') . '\/?/', $this->baseUrl, $path);
-
 	}
-
-
 }
